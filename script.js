a975a0ec699c6cedc7111ca3718361220fea5b95
Igoryn23 <igoryn9702@gmail.com>
2021-06-09 12:22:18 +0300

    add: array-3

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function minSum(arr) {
 }
 
 minSum([5, 4, 2, 3]);
+
+// An element is leader if it is greater than The Sum all the elements to its right side.
+// Given an array/list [] of integers , Find all the LEADERS in the array.
+
+
+var arrayLeaders = numbers => {
+    let arr = [];
+    let sumTemp = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        for (let k = i + 1; k < numbers.length; k++) {
+            sumTemp += numbers[k];
+        }
+        if (numbers[i] > sumTemp) {
+            arr.push(numbers[i]);
+        }
+        sumTemp = 0;
+    }
+    return arr;
+}
+
+arrayLeaders([1, 2, 3, 4, 0]);
